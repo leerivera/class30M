6ee7edcb2ad44b93bf836d1b7c77c77dee288eb0
leerivera <leerivera80@gmail.com>
2022-04-28 19:22:10 -0400

    yes

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,11 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class EspressoMachine{
+    constructor(color, make, model, price){
+        this.color= color
+        this.make = make
+        this.model = model
+        this.price = price
+
+
+    }
+}
